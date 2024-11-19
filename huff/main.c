3fc1d6ce05cff13ad1a7c83e76977fabfc794f86
Recep Aslantas <info@recp.me>
2024-11-19 14:53:30 +0300

    Initial Commit

---
+++ huff/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  huff
+//
+//  Created by Recep Aslantas on 11/19/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
